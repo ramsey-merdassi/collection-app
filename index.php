f658abaf3c277b6215ac5981e54fd9970a89a765
Ramsey Merdassi <merdassi.r@gmail.com>
2024-08-01 14:26:33 +0100

    removed font duplicate links and amended flex container

--- index.php
+++ index.php
@@ -13,11 +13,7 @@ require_once 'db.php';
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Anton&family=Oswald:wght@200..700&display=swap" rel="stylesheet">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Anton&family=Bitter:ital,wght@0,100..900;1,100..900&family=Oswald:wght@200..700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Anton&family=Bitter:ital,wght@0,100..900;1,100..900&family=Dancing+Script:wght@400..700&family=Noto+Sans+SC:wght@100..900&family=Oswald:wght@200..700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 </head>
 <body>
@@ -36,7 +32,7 @@ require_once 'db.php';
             {
                 echo
                     " <div class='destination_container'>
-                           <div class='location' style='background-image: url({$destination['image']})'><div class='destination_name'>".$destination['location']."</div></div>.
+                           <div class='location' style='background-image: url({$destination['image']})'><div class='destination_name'>".$destination['location']."</div></div>
                             <div class='stats'>
                                     <div class = 'inner_stats'>
                                         <div>
