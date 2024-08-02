942bed903e52d3f3e3a49e373a07fd6ca67f7df8
Ramsey Merdassi <merdassi.r@gmail.com>
2024-08-02 16:41:22 +0100

    applied trainer review recommendations

--- add_location.php
+++ add_location.php
@@ -10,18 +10,18 @@ if(isset($_POST['location'], $_POST['url'], $_POST['dish'], $_POST['attraction']
     $dish = $_POST['dish'];
     $attraction = $_POST['attraction'];
     $beer = $_POST['beer'];
-}
 
-$addToDB = $db->prepare('INSERT INTO `destination`(`location`, `top_dish`, `average_beer_price`, `top_attraction`, `image`)
+    $addToDB = $db->prepare('INSERT INTO `destination`(`location`, `top_dish`, `average_beer_price`, `top_attraction`, `image`)
                         VALUES (:location, :top_dish, :average_beer_price, :top_attraction, :image)');
 
-$addToDB->bindParam(':location', $location);
-$addToDB->bindParam(':top_dish', $dish);
-$addToDB->bindParam(':average_beer_price', $beer);
-$addToDB->bindParam(':top_attraction', $attraction);
-$addToDB->bindParam(':image', $url);
+    $addToDB->bindParam(':location', $location);
+    $addToDB->bindParam(':top_dish', $dish);
+    $addToDB->bindParam(':average_beer_price', $beer);
+    $addToDB->bindParam(':top_attraction', $attraction);
+    $addToDB->bindParam(':image', $url);
 
-$addToDB->execute();
+    $addToDB->execute();
+}
 
 header('Location: index.php');
 
